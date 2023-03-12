2341ef2f80cbf8656e6928476f435c9095b1185e
Jason Go <jasongo@jasongo.net>
2023-03-13 03:25:09 +0800

    fix types

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,9 @@ export interface fastaKjb {
     browserURL: URL;
     mykajabiURL: URL;
     content: Response | Promise<Response>;
+    [key: string]: any;
 }
 export interface fastaVariables {
     fastaKjb: fastaKjb;
+    [key: string]: any;
 }
--- index.ts
+++ index.ts
@@ -2,8 +2,10 @@ export interface fastaKjb {
   browserURL: URL
   mykajabiURL: URL
   content: Response | Promise<Response>
+  [key: string]: any
 }
 
 export interface fastaVariables {
   fastaKjb: fastaKjb
+  [key: string]: any
 }
